6e896cf35af802003f7d4f7d51cb1e439ffe4fcf
Avelon Pang <avelonpang@gmail.com>
2020-11-09 20:07:26 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,60 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
 
+require 'pry'
+
+# def find_item_by_name_in_collection(name, collection)
+#   collection.each do |item_info|
+#     if item_info[:item] == name
+#       return item_info
+#     end 
+#   end 
+#   nil
+# end 
+
+def find_item_by_name_in_collection(name, collection)
+  counter = 0 
+  while counter < collection.length 
+  if collection[counter][:item] == name
+  return collection[counter]
 end
+counter += 1 
+end
+#nil
+end 
+ 
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-end
-
-
+  new_cart = []
+  counter = 0 
+  while counter < cart.length 
+  new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+  if new_cart_item != nil 
+    new_cart_item[:count] += 1 
+  else
+    new_cart_item = {
+      :item => cart[counter][:item],
+      :price => cart[counter][:price], 
+      :clearance => cart[counter][:clearance],
+      :count => 1 
+    }
+    new_cart << new_cart_item 
+  end
+  counter += 1 
+end 
+new_cart 
+end 
   
+#   cart_arr = []
+#     #cart.each do |cart_item|  #cart_itme = hash 
+#     #cart_item.each do |name, data|
+#     # binding.pry 
+#       if cart_item[:item] == cart
+#         #cart_item.each do ||
+#         cart_arr[:count] += 1
+        
+#       else
+#         #cart_arr[name] = data
+#         cart_arr[:item][:count] = 1
+#       end
+#   cart_arr
+# end
+
